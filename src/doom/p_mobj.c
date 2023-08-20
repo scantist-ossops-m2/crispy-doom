65a0a953d7d3874c0f292842b959d013c9f1762f
Fabian Greffrath <fabian@greffrath.com>
2023-08-20 14:07:08 +0200

    comment

--- src/doom/p_mobj.c
+++ src/doom/p_mobj.c
@@ -257,6 +257,7 @@ void P_XYMovement (mobj_t* mo)
     if (mo->flags & (MF_MISSILE | MF_SKULLFLY) )
 	return; 	// no friction for missiles ever
 		
+  // [crispy] fix mid-air speed boost when using noclip cheat
   if (!player || !(player->mo->flags & MF_NOCLIP))
     if (mo->z > mo->floorz)
 	return;		// no friction when airborne
