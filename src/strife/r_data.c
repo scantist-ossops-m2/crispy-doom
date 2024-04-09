a711fe8dc6e583eaa18367ab68cdec4630b81223
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-04-09 23:07:30 +0300

    Simplify COLORMAP's brightmapped colors calculation

--- src/strife/r_data.c
+++ src/strife/r_data.c
@@ -727,11 +727,11 @@ void R_InitColormaps (void)
 				}
 				else
 				{
-					// [JN] Vanilla Strife is using COLORMAP indexes 224-255 without fading to black
+					// [crispy] Vanilla Strife is using COLORMAP columns 224-255 without fading to black
 					// so they work as brightmaps. To replicate it, lock such indexes on first light level.
-					r = gamma2table[crispy->gamma][playpal[3 * k + 0]] + gamma2table[crispy->gamma][0] * scale;
-					g = gamma2table[crispy->gamma][playpal[3 * k + 1]] + gamma2table[crispy->gamma][0] * scale;
-					b = gamma2table[crispy->gamma][playpal[3 * k + 2]] + gamma2table[crispy->gamma][0] * scale;
+					r = gamma2table[crispy->gamma][playpal[3 * k + 0]];
+					g = gamma2table[crispy->gamma][playpal[3 * k + 1]];
+					b = gamma2table[crispy->gamma][playpal[3 * k + 2]];
 				}
 
 				colormaps[j++] = 0xff000000 | (r << 16) | (g << 8) | b;
